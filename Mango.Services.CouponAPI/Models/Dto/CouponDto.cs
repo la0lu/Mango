20b57584d507526887c0566e059c031a615903b9
La0lu <oguntoyetobi456@gmail.com>
2023-11-12 05:12:24 +0100

    fix ignore

--- Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
+++ Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -1,7 +1,7 @@
 ﻿namespace Mango.Services.CouponAPI.Models.Dto
 {
     public class CouponDto
-    {
+    { 
         public int CouponId { get; set; }
         public string CouponCode { get; set; }
         public double DiscountAmount { get; set; }
